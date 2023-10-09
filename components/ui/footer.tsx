80c34c6a457809acaa9f6cb716fd8035360fb53e
xDev2022-power <22comzace@gmail.com>
2023-10-09 14:46:24 +0800

    1009 246pms

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-import OrcaLogoHeader from "../../app/src/assets/images/Orca.png"
+import OrcaLogoHeader from "../../app/src/assets/images/orca.png"
 
 export default function Footer() {
   return (
